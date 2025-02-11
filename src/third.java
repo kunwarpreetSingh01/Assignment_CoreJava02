751fc1b35b39a9d9e95ccaa41ea8e882ad4ea786
kunwarpreet1 <kunwarpreetpro@gmail.com>
2025-02-11 17:29:18 +0530

    modifying file

--- src/third.java
+++ src/third.java
@@ -1,14 +1,25 @@
 
 //Create a thread using lambda function
 
+class MyTask implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread running: " + i);
+            try {
+                Thread.sleep(500); 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
 public class third {
     public static void main(String[] args) {
-        Thread thread = new Thread(()->{
-           for(int i=1;i<=5;i++){
-               System.out.println("Thread is Running" + i);
-           }
-
-        });
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task);
+        thread.start();
     }
 }
+
